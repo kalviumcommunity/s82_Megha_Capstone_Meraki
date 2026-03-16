acede59eb44c5c4ddfb8b9bdfd4d990906d7c5ff
Megha R <megha202005@gmail.com>
2026-03-16 16:58:40 +0530

    Delete meraki-backend directory

--- meraki-backend/models/NGO.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ngoSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  location: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('NGO', ngoSchema);
--- meraki-backend/models/Volunteer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const volunteerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  skills: [String],
-  availability: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Volunteer', volunteerSchema);
